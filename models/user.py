4763827e612f84a521dbbddb7afaf3b056d66e51
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-02-20 10:21:30 -0800

    correct relation class

--- models/user.py
+++ models/user.py
@@ -15,4 +15,4 @@ class User(BaseModel, Base):
     first_name = Column(String(128))
     last_name = Column(String(128))
     places = relationship("Place", cascade="all, delete", backref="user")
-    reviews = relationship("User", cascade="all, delete", backref="user")
+    reviews = relationship("Review", cascade="all, delete", backref="user")
