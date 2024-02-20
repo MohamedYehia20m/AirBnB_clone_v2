f133b33e74cd14b42365da67b11bde0fbd6a7627
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-02-20 06:09:24 -0800

    change permissions

--- models/amenity.py
+++ models/amenity.py
@@ -3,5 +3,8 @@
 from models.base_model import BaseModel
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
+    place_amenities = # TODO: class attribute place_amenities must represent a relationship Many-To-Many between the class Place and Amenity. Please see below more detail: place_amenity in the Place update
