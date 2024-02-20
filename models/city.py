61d1221a3da5dad73f8117f2bdbe5951619109b0
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-02-20 03:51:26 -0800

    DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -2,8 +2,11 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel
 
+from sqlalchemy.orm import relationship
+
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
+    places = relationship("Place", cascade="all, delete-orphan", backref="cities")
--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel
 
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
@@ -13,3 +14,4 @@ class User(BaseModel):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", cascade="all, delete-orphan", backref="user")
